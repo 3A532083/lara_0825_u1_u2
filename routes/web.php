26364d8002c314e4e34c87df4e86ecfe4752f071
3A532083 <luckyday604@gm.student.ncut.edu.tw>
2018-12-07 23:22:23 +0800

    練習3.4

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,12 @@ Route::get('/', function () {
 //    $post->save();
 
     // $posts = \App\Post::all();
+    //dd($posts);
 
-    $post = \App\Post::find(1);
-    dd($posts);
+//    $post = \App\Post::find(1);
+//    dd($post);
+
+    $post=\App\Post::where('id', '<', 10)->orderBy('id','DESC')->get();
+    dd($post);
+
 });
